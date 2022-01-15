13bda58f703b2a22f54f35c13158a294adf6ca8f
nabiladigo <nabiladigo91@gmail.com>
2022-01-15 14:22:11 -0500

    update ejs files

---
+++ models/products.js
@@ -0,0 +1,157 @@
+class Collection {
+  #Model
+  #currentId
+  #items
+  constructor(model, startingData) {
+      this.#Model = model;
+      this.#currentId = 0;
+      this.#items = this.#populateItems( startingData );
+  }
+
+  /**
+   * @description It will take an array as a argument 
+   * @returns on Object that contains the { id as a key } and { te item as the value } 
+   */
+
+  #populateItems( startingData ) {
+      return startingData.reduce(( acc, item, idx ) => {
+          this.#currentId = idx;
+          acc[this.#currentId] = new this.#Model(item, idx)
+          return acc;
+      }, {});
+  }
+
+  #generateId(){
+      return ++this.#currentId
+  }
+
+  /**
+   * @description Will return an array with all items availible in this.items
+   * @returns array
+   */
+
+  find() {
+      return Object.values(this.#items);
+  }
+
+  /**
+   * @description Will return item match with the itemId
+   * @param { string } itemId
+   * @param { function } callBack Will return error or item
+   * @returns function;
+   */
+
+  findById( itemId, callBack ) {
+      if (!itemId) return console.log("missing id in first argument");
+  
+      if (typeof callBack !== "function") {
+          return console.log("missing function in second argument");
+      }
+  
+      let error;
+      const item = this.#items[itemId];
+  
+      if (!item) {
+          error = { message: `item with id "${itemId}" can't be found` };
+      }
+  
+      return callBack(error, item);
+  }
+
+  create( data, callBack ) {
+      if (!data) return console.log("missing data in first argument");
+  
+      if (typeof callBack !== "function") {
+          return console.log("missing function in second argument");
+      }
+  
+      let error, newItem;
+  
+      const isEmpty = Object.keys(data).every(field => data[field] === "");
+  
+      if (isEmpty) {
+          error = { message: `you have empty fields` };
+      } else {
+          
+          newItem = new this.#Model( data, this.#generateId());
+  
+          this.#items[newItem.id] = newItem;
+      }
+  
+      return callBack(error, newItem);
+  }
+};
+
+class Product {
+  constructor( data, id ) {
+      this.id = id;
+      this.name = data.name;
+      this.price = data.price;
+      this.image = data.image;
+  }
+}
+
+
+module.exports = new Collection(Product, [
+  { 
+    name: "Car", 
+    price: 20000, 
+    img: "https://static4.arrow.com/-/media/arrow/images/1400-x-1400/critical-components_image-1/f1-race-car.jpeg",  
+  },
+  { 
+    name: "Cat", 
+    price: 100, 
+    img: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/best-girl-cat-names-1606245046.jpg?crop=0.668xw:1.00xh;0.126xw,0&resize=640:*",
+  },
+  { 
+    name: "Crab", 
+    price: 2, 
+    img: "http://scontent.cdninstagram.com/t51.2885-19/s150x150/13402342_1111471978911960_1380878568_a.jpg",
+  },
+  { 
+    name: "Crib", 
+    price: 200, 
+    img: "https://s-media-cache-ak0.pinimg.com/originals/99/29/ee/9929eef9086e07bd7e50102bc37ff3a8.jpg"
+  },
+  { 
+    name: "Coat", 
+    price: 200, 
+    img: "http://cooljunkyouneed.com/4548/uploads/2014/02/Workaholics-Bear-Coat-150x150.jpg"
+  },
+  { 
+    name: "Cake", 
+    price: 3, 
+    img: "http://scontent.cdninstagram.com/t51.2885-19/s150x150/11356601_447610668772561_439752401_a.jpg"
+  },
+  { 
+    name: "Concussion", 
+    price: 0, 
+    img: "http://youngmenshealthsite.org/wp-content/uploads/2015/05/concussion1-150x150.jpg"
+  },
+  { 
+    name: "Coal", 
+    price: 1, 
+    img: "http://www.whitecatpublications.com/wp-content/uploads/2010/12/lump-of-coal-150x150.jpg"
+  },
+  { 
+    name: "Cyclone", 
+    price: 70000, 
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/Cyclone_Mala.JPG/150px-Cyclone_Mala.JPG"
+  },
+  { 
+    name: "Career", 
+    price: 13500, 
+    img: "http://waterfordwhispersnews.com/wp-content/uploads/2014/10/happy-worker-e1412334561186-150x150.jpg"
+  },
+  { 
+    name: "Cillian Murphy", 
+    price: 400, 
+    img: "http://static.buzznet.com/uploads/2012/03/msg-133176055505-150x150.jpg"
+  },
+  { 
+    name: "Climate Change", 
+    price: 9, 
+    img: "http://scitechdaily.com/images/Detailed-Global-Climate-Change-Projections-150x150.jpg" 
+  }
+]
+);
--- server.js
+++ server.js
@@ -1,17 +1,33 @@
 const express = require('express');
 const app = express();
-
+// db
+const products= require('./models/products.js');
+//  variables
 const PORT = 3000;
 
-// middleware
-app.use(express.urlencoded({ extended: false }));
+//  set our app need to make sure if its views or view 
 
-// db
-const products = require('./products');
+app.set('view engine', 'ejs')
+//   i might need this to make ejs work dont know yet 
+// app.engine('ejs', require('ejs').__express);
+
+// middleware 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static('public'));
+
+app.use((req, res, next) => {    
+  // console.log("I'm running for another new route")
+// console.log(`${req.method} ${req.originalUrl}`);    
+next();
+});
 
 // product index route
+
 app.get('/products', (req, res) => {
-  res.send(products);
+  const allProducts = products.find();
+  const items = {products: allProducts};
+
+  res.render('index.ejs', items);
 });
 
 // product show route
@@ -27,6 +43,41 @@ app.post('/products', (req, res) => {
   res.redirect('/products');
 });
 
+app.get('/products/new', (req, res) =>{
+  res.render('new.ejs')
+})
+
+// this route will catch GET requests to /products/index/ and respond with a single product
+app.get('/products/:productId', (req, res) => {
+    
+  products.findById(req.params.productId, (error, foundProduct) => {
+      if (error) {
+          console.log(error);
+          req.error = error;
+          return next();
+      }
+      /* 
+      1. the first param of render() is the .ejs file 
+      that we want to inject data into.
+      
+      2. the second param is the data that we want 
+      to inject into the .ejs file (it must be an object)
+      */
+
+      /*	
+      there will be a variable available inside
+      the show.ejs file called product, 
+      and its value the foundItem
+     */
+      res.render('show.ejs', {product: foundProduct});
+  });
+  
+});
+
+app.get("/*", (req, res) => {
+  const context = { error: req.error };
+  return res.status(404).render("404", context);
+});
 
 app.listen(PORT, () => {
   console.log("App is running on port: ", PORT);
